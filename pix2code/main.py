108878e971ab00e5d0b76ed95826fde64c45d2ef
Ned Ruggeri <ruggeri@self-loop.com>
2018-05-03 10:59:30 -0700

    begin suggesting future argparsing

--- pix2code/main.py
+++ pix2code/main.py
@@ -1,15 +1,22 @@
 import argparse
 import sys
 
-from .subcommands import compile
-
 def main():
   parser = argparse.ArgumentParser(description = "pix2code")
-  subparsers = parser.add_subparsers(dest = "command_name")
+  subparsers = parser.add_subparsers(dest = "subcommand", metavar = "subcommand")
 
-  compile.configure_parser(subparsers.add_parser("compile"))
+  from .subcommands import compile
+  compile.configure_parser(subparsers.add_parser("compile", help = "compiles a .gui file"))
+
+  # TODO: train
+  subparsers.add_parser("train", help = "trains a model on a dataset")
+
+  # TODO: predict
+  subparsers.add_parser("predict", help = "generates gui code from an image")
 
   args = parser.parse_args()
 
-  if args.command_name == "compile":
+  if args.subcommand == None:
+    parser.print_help()
+  elif args.subcommand == "compile":
     compile.main(args)
